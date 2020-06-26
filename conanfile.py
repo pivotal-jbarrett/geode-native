1c8eb58cfbae79065946fc28703da0d84fd37a7c
Jacob Barrett <jbarrett@pivotal.io>
2020-06-26 12:22:41 -0700

    Use conan to get GTest dependency

---
+++ conanfile.py
@@ -0,0 +1,7 @@
+from conans import ConanFile, CMake, tools
+
+
+class ApacheGeodeNative(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    requires = "gtest/1.10.0"
+    generators = "cmake_find_package_multi", "cmake_paths"
