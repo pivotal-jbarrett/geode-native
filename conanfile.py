041b87d7a3973741f91604a2d28660bd29e47b9a
Jacob Barrett <jbarrett@pivotal.io>
2020-06-26 14:09:30 -0700

    Use conan to get benchmark dependency

--- conanfile.py
+++ conanfile.py
@@ -3,5 +3,5 @@ from conans import ConanFile, CMake, tools
 
 class ApacheGeodeNative(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = "gtest/1.10.0"
+    requires = "gtest/1.10.0", "benchmark/1.5.0"
     generators = "cmake_find_package_multi", "cmake_paths"
