9b7530914d18b5bbd3fac284c39cc8fdd8dfd66a
Jacob Barrett <jbarrett@pivotal.io>
2020-06-26 17:00:18 -0700

    Use conan to get doxygen dependency

--- conanfile.py
+++ conanfile.py
@@ -4,5 +4,5 @@ from conans import ConanFile, CMake, tools
 class ApacheGeodeNative(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     requires = "gtest/1.10.0", "benchmark/1.5.0", "sqlite3/3.32.3"
-    build_requires = "doxygen_installer/1.8.17"
+    build_requires = "doxygen/1.8.17"
     generators = "cmake_find_package_multi", "cmake_paths"
